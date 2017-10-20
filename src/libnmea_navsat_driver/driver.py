9d93f8aa80d72f02a92766738ffd2b613f1eb82f
Ren Ye <renye.china@gmail.com>
2017-10-20 23:19:35 +0800

    fix zero altitude

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -146,7 +146,7 @@ class RosNMEADriver(object):
                     longitude = -longitude
                 current_fix.longitude = longitude
 
-                current_fix.altitude = float('NaN')
+                current_fix.altitude = 0. # float('NaN')
                 current_fix.position_covariance_type = \
                     NavSatFix.COVARIANCE_TYPE_UNKNOWN
 
